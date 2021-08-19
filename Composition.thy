6f7a6e2f7de3c177661d4f3a641d378f33305f9d
Jan van Brügge <supermanitu@gmail.com>
2021-08-19 11:33:45 +0200

    Create tactic to solve MRBNF axioms with BNF

--- Composition.thy
+++ Composition.thy
@@ -1,5 +1,5 @@
 theory Composition
-  imports "thys/Prelim" "HOL-Cardinals.Bounded_Set"
+  imports "thys/Prelim"
   keywords
     "print_mrbnfs" :: diag and
     "mrbnf" :: thy_goal
@@ -24,12 +24,34 @@ typedef ('tyvar, 'btyvar, 'c, 'd) pre_\<tau> = "{ x. x \<in> (UNIV :: ('tyvar + 
 
 print_bnfs
 
-ML \<open>
-
-val y = fn x => MRBNF_Def.mrbnf_of_bnf x @{thm infinite_regular_card_order_natLeq}
-\<close>
 declare [[bnf_internals]]
 datatype ('a, 'b) sum2 = L 'a | R 'b
+
+print_theorems
+
+ML \<open>
+val test = MRBNF_Def.mrbnf_def MRBNF_Def.Do_Inline (K MRBNF_Def.Dont_Note) false I []
+val SOME a = BNF_Def.bnf_of @{context} "Composition.sum2"
+
+fun pretty_thm ctxt thm = Syntax.pretty_term ctxt (Thm.prop_of thm)
+fun pretty_terms ctxt trms = Pretty.block (Pretty.commas (map (Syntax.pretty_term ctxt) trms))
+fun pretty_thms ctxt thms = pretty_terms ctxt (map Thm.prop_of thms)
+fun pretty_cterm ctxt ctrm = Syntax.pretty_term ctxt (Thm.term_of ctrm)
+fun pretty_cterms ctxt ctrms = Pretty.block (Pretty.commas (map (pretty_cterm ctxt) ctrms))
+
+fun bnf_tac ctxt bnf = (
+        resolve_tac ctxt [BNF_Def.map_id0_of_bnf bnf]
+  THEN' resolve_tac ctxt [BNF_Def.map_comp0_of_bnf bnf]
+  THEN' SUBGOAL (K (resolve_tac ctxt [BNF_Def.map_cong0_of_bnf bnf] 1 THEN REPEAT (Goal.assume_rule_tac ctxt 1)))
+  THEN' RANGE (map (fn thm => resolve_tac ctxt [thm]) (BNF_Def.set_map0_of_bnf bnf))
+  THEN' (resolve_tac ctxt [@{thm infinite_regular_card_order_card_suc}] THEN' resolve_tac ctxt [BNF_Def.bd_card_order_of_bnf bnf] THEN' resolve_tac ctxt [BNF_Def.bd_Cinfinite_of_bnf bnf])
+  THEN' RANGE (map (fn thm => resolve_tac ctxt [@{thm card_suc_greater_set}] THEN' resolve_tac ctxt [BNF_Def.bd_card_order_of_bnf bnf] THEN' resolve_tac ctxt [thm]) (BNF_Def.set_bd_of_bnf bnf))
+  THEN' resolve_tac ctxt [BNF_Def.le_rel_OO_of_bnf bnf]
+  THEN' (K (Local_Defs.unfold_tac ctxt [BNF_Def.map_id_of_bnf bnf, BNF_Def.in_rel_of_bnf bnf, @{thm Set.mem_Collect_eq}]) THEN' resolve_tac ctxt [@{thm refl}])
+)
+\<close>
+
+find_theorems "card_order"
 
 print_theorems
 declare [[mrbnf_internals]]
@@ -38,16 +60,12 @@ mrbnf sum2_mr: "('a, 'b) sum2"
   sets:
     live: set1_sum2
     live: set2_sum2
-  bd: natLeq
-           apply (rule sum2.map_id0)
-          apply (rule sum2.map_comp0)
-  subgoal for x f1 f2 g1 g2 using sum2.map_cong0[of x f1 g1 f2 g2] .
-        apply (rule sum2.set_map0(1))
-       apply (rule sum2.set_map0(2))
-      apply (rule infinite_regular_card_order_natLeq)
-     defer defer defer
-     apply (simp add: sum2.map_id)
-  sorry
+  bd: "card_suc natLeq"
+  rel: rel_sum2
+           apply (tactic \<open>bnf_tac @{context} a 1\<close>)
+  done
+
+print_theorems
 
 ML \<open>
 val SOME x = BNF_Def.bnf_of @{context} "Sum_Type.sum"

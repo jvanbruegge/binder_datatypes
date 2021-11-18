f754933d23ccf9a52d6b7202f8c1e14a3f4daac2
Jan van Brügge <supermanitu@gmail.com>
2021-11-18 16:50:21 +0100

    Define the map function of the composed MRBNF

--- Composition.thy
+++ Composition.thy
@@ -6,25 +6,45 @@ datatype \<kappa> =
   Star ("\<star>")
   | KArrow \<kappa> \<kappa> (infixr "\<rightarrow>" 50)
 
-typedef ('tyvar, 'btyvar, 'c, 'd) pre_\<tau> = "{ x :: 'tyvar + unit + 'd * 'd + 'btyvar * \<kappa> * 'c. True }"
-  by simp
 (*
   TyVar 'tyvar
   | TyArrow
   | TyApp 'd 'd
   | TyForall 'btyvar \<kappa> 'c
 *)
+ML \<open>
+val systemf_type = @{typ "'tyvar + unit + 'd * 'd + 'btyvar * \<kappa> * 'c"}
+val systemf_type_vars = {
+  lives = [("''d", @{sort type}), ("'c", @{sort type})],
+  frees = [("'tyvar", @{sort type})],
+  bounds = [("'btyvar", @{sort type})],
+  deads = []
+}
+\<close>
 
 print_mrbnfs
 print_bnfs
 
+local_setup \<open>snd o the o MRBNF_Comp.as_mrbnf "Sum_Type.sum"\<close>
+local_setup \<open>snd o the o MRBNF_Comp.as_mrbnf "List.list"\<close>
 print_mrbnfs
+
+ML \<open>
+val sum = the (MRBNF_Def.mrbnf_of @{context} \<^type_name>\<open>sum\<close>)
+val list = the (MRBNF_Def.mrbnf_of @{context} \<^type_name>\<open>list\<close>)
+\<close>
+
+local_setup \<open>fn lthy => let
+  val (mrbnf, (_, lthy')) = MRBNF_Comp.clean_compose_mrbnf MRBNF_Def.Dont_Inline I Binding.empty
+                              sum [MRBNF_Comp.ID_mrbnf, list] ({map_unfolds = [], set_unfoldss = [], rel_unfolds = []}, lthy)
+  in lthy' end
+\<close>
 
 ML \<open>
 val test = @{typ "unit + unit + 'a list"}
 \<close>
 
-local_setup \<open>snd o MRBNF_Comp.mrbnf_of_typ { lives = [("'a", dummyS)], frees = [], bounds = [], deads = [] } test\<close>
+local_setup \<open>snd o MRBNF_Comp.mrbnf_of_typ { lives = [("'a", @{sort type})], frees = [], bounds = [], deads = [] } test\<close>
 
 print_mrbnfs
 

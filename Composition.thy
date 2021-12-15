5110e8ecb8ba5c8ee2bccd24628d24692d1dffc8
Jan van Brügge <supermanitu@gmail.com>
2021-12-15 14:29:21 +0100

    Fix mrbnf_def for non-trivial cases

--- Composition.thy
+++ Composition.thy
@@ -22,20 +22,70 @@ val systemf_type_vars = {
 }
 \<close>
 
+datatype (setF1_F: 'a, setF2_F': 'a', setL3_F: 'x, setB4_F: 'b, setB5_F: 'b', setL6_F: 'c, setL7_F: 'd, setL8_F: 'e, setL9_F: 'f) F_raw =
+  E "'x + 'a + ('a' * 'b') * 'c * 'd + 'a' * 'f"
+  for map: map_F rel: rel_F
+type_synonym ('a, 'a', 'x, 'b, 'b', 'c, 'd, 'e, 'f) F = "('a, 'a', 'x, 'b, 'b', 'c, 'd, 'e, 'f) F_raw"
+
+datatype (setF1_G: 'a, setF2_G': 'a', setL3_G: 'y, setB4_G: 'b, setB5_G: 'b', setL6_G: 'g) G_raw =
+  E "'y + 'a + ('a' * 'b') * 'y * 'g + 'a' * 'g"
+  for map: map_G rel: rel_G
+type_synonym ('a, 'a', 'y, 'b, 'b', 'g) G = "('a, 'a', 'y, 'b, 'b', 'g) G_raw"
+
 print_mrbnfs
 print_bnfs
 
-local_setup \<open>snd o the o MRBNF_Comp.as_mrbnf "Sum_Type.sum"\<close>
-local_setup \<open>snd o the o MRBNF_Comp.as_mrbnf "List.list"\<close>
+local_setup \<open>snd o the o MRBNF_Def.as_mrbnf "Sum_Type.sum"\<close>
+local_setup \<open>snd o the o MRBNF_Def.as_mrbnf "List.list"\<close>
 print_mrbnfs
+
+mrbnf F: "('a, 'a', 'x, 'b, 'b', 'c, 'd, 'e, 'f) F"
+  map: "map_F"
+  sets:
+   free: "setF1_F :: _ \<Rightarrow> 'a set" 
+   free: "setF2_F' :: _ \<Rightarrow> 'a' set"
+   live: "setL3_F :: _ \<Rightarrow> 'x set"
+   bound: "setB4_F :: _ \<Rightarrow> 'b set"
+   bound: "setB5_F :: _ \<Rightarrow> 'b' set"
+   live: "setL6_F :: _ \<Rightarrow> 'c set" 
+   live: "setL7_F :: _ \<Rightarrow> 'd set" 
+   live: "setL8_F :: _ \<Rightarrow> 'e set" 
+   live: "setL9_F :: _ \<Rightarrow> 'f set"
+  bd: "natLeq"
+  wits: "F_raw.E o Inl"
+  rel: "\<lambda>X. rel_F (=) (=) X (=) (=)"
+  pred: "\<lambda>X. pred_F_raw (\<lambda>_. True) (\<lambda>_. True) X (\<lambda>_. True) (\<lambda>_. True)"
+  sorry
+
+mrbnf G: "('a, 'a', 'y, 'b, 'b', 'g) G_raw"
+  map: "map_G"
+  sets:
+    free: "setF1_G :: _ \<Rightarrow> 'a set" 
+    free: "setF2_G' :: _ \<Rightarrow> 'a' set"
+    live: "setL3_G :: _ \<Rightarrow> 'y set"
+    bound: "setB4_G :: _ \<Rightarrow> 'b set" 
+    bound: "setB5_G :: _ \<Rightarrow> 'b' set"
+    live: "setL6_G :: _ \<Rightarrow> 'g set"
+  bd: "natLeq"
+  wits: "G_raw.E o Inl"
+  rel: "\<lambda>X. rel_G (=) (=) X (=) (=)"
+  pred: "\<lambda>X. pred_G_raw (\<lambda>_. True) (\<lambda>_. True) X (\<lambda>_. True) (\<lambda>_. True)"
+  sorry
 
 ML \<open>
 val sum = the (MRBNF_Def.mrbnf_of @{context} \<^type_name>\<open>sum\<close>)
 val list = the (MRBNF_Def.mrbnf_of @{context} \<^type_name>\<open>list\<close>)
 \<close>
 
+ML \<open>
+Multithreading.parallel_proofs := 0;
+\<close>
+
+ML_file \<open>./Tools/mrbnf_comp_tactics.ML\<close>
+ML_file \<open>./Tools/mrbnf_comp.ML\<close>
+
 local_setup \<open>fn lthy => let
-  val (mrbnf, (_, lthy')) = MRBNF_Comp.clean_compose_mrbnf MRBNF_Def.Dont_Inline I Binding.empty
+  val (_, (_, lthy')) = MRBNF_Comp.clean_compose_mrbnf MRBNF_Def.Do_Inline I @{binding foo}
                               sum [MRBNF_Comp.ID_mrbnf, list] ({map_unfolds = [], set_unfoldss = [], rel_unfolds = []}, lthy)
   in lthy' end
 \<close>

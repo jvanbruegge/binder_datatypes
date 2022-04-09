c9ae4c577873d6d7a55d0a76aed40bfa464de833
Jan van Brügge <supermanitu@gmail.com>
2022-04-09 14:13:32 +0200

    WIP: work on thesis proofs

--- tests/MRBNF_Examples.thy
+++ tests/MRBNF_Examples.thy
@@ -18,61 +18,61 @@ declare [[quick_and_dirty=true]]
 mrbnf F: "('a, 'a', 'x, 'b, 'b', 'c, 'd, 'e, 'f) F"
   map: "map_F"
   sets:
-   free: "setF1_F :: _ ⇒ 'a set"
-   free: "setF2_F :: _ ⇒ 'a' set"
-   live: "setL3_F :: _ ⇒ 'x set"
-   bound: "setB4_F :: _ ⇒ 'b set"
-   bound: "setB5_F :: _ ⇒ 'b' set"
-   live: "setL6_F :: _ ⇒ 'c set"
-   live: "setL7_F :: _ ⇒ 'd set"
-   live: "setL8_F :: _ ⇒ 'e set"
-   live: "setL9_F :: _ ⇒ 'f set"
+   free: "setF1_F :: _ \<Rightarrow> 'a set"
+   free: "setF2_F :: _ \<Rightarrow> 'a' set"
+   live: "setL3_F :: _ \<Rightarrow> 'x set"
+   bound: "setB4_F :: _ \<Rightarrow> 'b set"
+   bound: "setB5_F :: _ \<Rightarrow> 'b' set"
+   live: "setL6_F :: _ \<Rightarrow> 'c set"
+   live: "setL7_F :: _ \<Rightarrow> 'd set"
+   live: "setL8_F :: _ \<Rightarrow> 'e set"
+   live: "setL9_F :: _ \<Rightarrow> 'f set"
   bd: "natLeq"
   wits: "F.E o Inl"
-  rel: "λX. rel_F (=) (=) X (=) (=)"
-  pred: "λX. pred_F (λ_. True) (λ_. True) X (λ_. True) (λ_. True)"
+  rel: "\<lambda>X. rel_F (=) (=) X (=) (=)"
+  pred: "\<lambda>X. pred_F (\<lambda>_. True) (\<lambda>_. True) X (\<lambda>_. True) (\<lambda>_. True)"
   sorry
 print_theorems
 
 mrbnf F': "('a, 'a', 'x, 'b, 'c, 'd) F'"
   map: "map_F'"
   sets:
-   bound: "setF1_F' :: _ ⇒ 'a set"
-   bound: "setF2_F' :: _ ⇒ 'a' set"
-   live: "setL3_F' :: _ ⇒ 'x set"
-   free: "setB4_F' :: _ ⇒ 'b set"
-   live: "setL5_F' :: _ ⇒ 'c set"
-   live: "setL6_F' :: _ ⇒ 'd set"
+   bound: "setF1_F' :: _ \<Rightarrow> 'a set"
+   bound: "setF2_F' :: _ \<Rightarrow> 'a' set"
+   live: "setL3_F' :: _ \<Rightarrow> 'x set"
+   free: "setB4_F' :: _ \<Rightarrow> 'b set"
+   live: "setL5_F' :: _ \<Rightarrow> 'c set"
+   live: "setL6_F' :: _ \<Rightarrow> 'd set"
   bd: "card_suc natLeq"
   wits: "F'.E o Inl"
-  rel: "λX. rel_F' (=) (=) X (=)"
-  pred: "λX. pred_F' (λ_. True) (λ_. True) X (λ_. True)"
+  rel: "\<lambda>X. rel_F' (=) (=) X (=)"
+  pred: "\<lambda>X. pred_F' (\<lambda>_. True) (\<lambda>_. True) X (\<lambda>_. True)"
   sorry
 
 mrbnf G: "('a, 'a', 'y, 'b, 'b', 'g) G"
   map: "map_G"
   sets:
-    free: "setF1_G :: _ ⇒ 'a set"
-    free: "setF2_G :: _ ⇒ 'a' set"
-    live: "setL3_G :: _ ⇒ 'y set"
-    bound: "setB4_G :: _ ⇒ 'b set"
-    bound: "setB5_G :: _ ⇒ 'b' set"
-    live: "setL6_G :: _ ⇒ 'g set"
+    free: "setF1_G :: _ \<Rightarrow> 'a set"
+    free: "setF2_G :: _ \<Rightarrow> 'a' set"
+    live: "setL3_G :: _ \<Rightarrow> 'y set"
+    bound: "setB4_G :: _ \<Rightarrow> 'b set"
+    bound: "setB5_G :: _ \<Rightarrow> 'b' set"
+    live: "setL6_G :: _ \<Rightarrow> 'g set"
   bd: "natLeq"
   wits: "G.E o Inl"
-  rel: "λX. rel_G (=) (=) X (=) (=)"
-  pred: "λX. pred_G (λ_. True) (λ_. True) X (λ_. True) (λ_. True)"
+  rel: "\<lambda>X. rel_G (=) (=) X (=) (=)"
+  pred: "\<lambda>X. pred_G (\<lambda>_. True) (\<lambda>_. True) X (\<lambda>_. True) (\<lambda>_. True)"
   sorry
 declare [[quick_and_dirty=false]]
 
-ML ‹
-val f = the (MRBNF_Def.mrbnf_of @{context} "Composition.F")
-val f' = the (MRBNF_Def.mrbnf_of @{context} "Composition.F'")
-val g = the (MRBNF_Def.mrbnf_of @{context} "Composition.G")
-›
+ML \<open>
+val f = the (MRBNF_Def.mrbnf_of @{context} "MRBNF_Examples.F")
+val f' = the (MRBNF_Def.mrbnf_of @{context} "MRBNF_Examples.F'")
+val g = the (MRBNF_Def.mrbnf_of @{context} "MRBNF_Examples.G")
+\<close>
 
 declare [[ML_print_depth=100000]]
-local_setup ‹fn lthy =>
+local_setup \<open>fn lthy =>
   let
     val Xs = map dest_TFree [@{typ 'x}]
     val Ts = [@{typ 'a}, @{typ 'b}, @{typ 'c}, @{typ 'd}, @{typ 'e}, @{typ 'f}, @{typ 'g}, @{typ 'h}, @{typ 'i}]
@@ -94,6 +94,11 @@ local_setup ‹fn lthy =>
     [] (fst tys) mrbnf lthy'
   val _ = @{print} b
   in snd b
-  end›
+  end\<close>
+
+term "True"
+
+term "\<Union> (setL3_G (map_G id id setF1_F' id id setF1_F' x))"
+term "\<Union> (image setF1_F' (setL3_G x)) \<union> \<Union> (image setF1_F' (setL6_G x))"
 
 end

21245d8ebf6db88f4f481df7d906f3d6f36e80e8
Jan van Brügge <supermanitu@gmail.com>
2023-04-16 12:24:31 +0100

    Integrate tvsubst into create_binder_datatype

--- thys/MRBNF_Recursor.thy
+++ thys/MRBNF_Recursor.thy
@@ -257,6 +257,6 @@ lemma supp_swap_bound: "infinite (UNIV :: 'a set) \<Longrightarrow> |supp (id (x
     (auto simp: cinfinite_imp_infinite)
 lemma UN_single: "\<Union>(f ` {a}) = f a" by simp
 
-ML_file \<open>../Tools/mrbnf_tvsubst.ML\<close>
+(*ML_file \<open>../Tools/mrbnf_tvsubst.ML\<close>*)
 
 end

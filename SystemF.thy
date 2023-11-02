764d5cd677205d2c98f9a5f978069d954f335aef
Jan van Brügge <supermanitu@gmail.com>
2023-11-02 17:24:59 +0000

    WIP

--- SystemF.thy
+++ SystemF.thy
@@ -82,6 +82,7 @@ val spec = {
 
 declare [[ML_print_depth=10000]]
 declare [[mrbnf_internals]]
+ML \<open>Multithreading.parallel_proofs := 0\<close>
 local_setup \<open>fn lthy =>
 let
   val lthy' = MRBNF_Sugar.create_binder_datatype spec lthy
@@ -89,9 +90,96 @@ in lthy' end\<close>
 print_theorems
 print_mrbnfs
 
+(* ??.SystemF.FFVars_term2 (??.SystemF.tvsubst f2 t) =
+ (SUP a\<in>??.SystemF.FFVars_term2 t. ??.SystemF.FFVars_term2 (f2 a) *)
+
 thm term.strong_induct
 thm term.set
 thm term.map
 thm term.subst
+
+lemma FFVars_tvsubst:
+  fixes t::"('a::var_term_pre, 'b::var_term_pre) term"
+  assumes "|tvSSupp_tvsubst f2| <o cmin |UNIV::'a set| |UNIV::'b set|"
+    shows "FFVars_term2 (tvsubst f2 t) = (\<Union>a\<in>FFVars_term2 t. FFVars_term2 (f2 a))"
+  apply (rule term.TT_fresh_co_induct[of "tvIImsupp1_tvsubst f2" "tvIImsupp2_tvsubst f2" _ t])
+    apply (unfold tvIImsupp1_tvsubst_def comp_def)[1]
+    apply ((rule assms term_pre.Un_bound term_pre.UNION_bound term.set_bd_UNIV ordLess_ordLeq_trans cmin1 cmin2 card_of_Card_order)+)[1]
+   apply (unfold tvIImsupp2_tvsubst_def comp_def)[1]
+   apply ((rule assms term_pre.Un_bound term_pre.UNION_bound term.set_bd_UNIV ordLess_ordLeq_trans cmin1 cmin2 card_of_Card_order)+)[1]
+  subgoal for v
+    apply (rule case_split[of "tvisVVr_tvsubst (term_ctor v)"])
+     apply (unfold tvisVVr_tvsubst_def)[1]
+     apply (erule exE)
+    subgoal premises prems for a
+      apply (subst prems(9))+
+      apply (subst term.tvsubst_VVr)
+       apply (rule assms)
+      apply (subst term.FVars_VVr)
+      apply (unfold UN_single)[1]
+      apply (rule refl)
+      done
+    apply (subst term.tvsubst_cctor_not_isVVr)
+         apply (rule assms)
+      (* REPEAT_DETERM *)
+        apply (rule iffD2[OF disjoint_iff])
+        apply (rule allI)
+        apply (rule impI)
+        apply assumption
+      (* copied from above *)
+       apply (rule iffD2[OF disjoint_iff])
+       apply (rule allI)
+       apply (rule impI)
+       apply assumption
+    (* copied from above *)
+      apply (rule iffD2[OF meta_eq_to_obj_eq[OF noclash_term_def]])
+      apply (unfold Int_Un_distrib Un_empty)[1]
+      apply (rule conjI)+
+       apply (rule iffD2[OF disjoint_iff])
+       apply (rule allI)
+        apply (rule impI)
+        apply assumption
+       apply (rule iffD2[OF disjoint_iff])
+       apply (rule allI)
+       apply (rule impI)
+       apply (unfold UN_iff Set.bex_simps)
+       apply (rule ballI)
+       apply assumption
+    apply (rule conjI)+
+       apply (rule iffD2[OF disjoint_iff])
+       apply (rule allI)
+        apply (rule impI)
+       apply assumption
+       apply (rule iffD2[OF disjoint_iff])
+       apply (rule allI)
+        apply (rule impI)
+       apply (unfold UN_iff Set.bex_simps)
+       apply (rule ballI)
+      apply assumption
+  (* END REPEAT_DETERM *)
+     apply assumption
+    apply (unfold term.FFVars_cctors)
+    apply (subst term_pre.set_map, (rule supp_id_bound bij_id)+)+
+    apply (unfold image_id image_comp[unfolded comp_def] UN_Un term.not_isVVr_free UN_empty Un_empty_left)
+    apply (rule arg_cong2[of _ _ _ _ "(\<union>)"])+
+      (* REPEAT_DETERM *)
+     apply (rule trans[rotated])
+      apply (rule sym)
+      apply (rule term.IImsupp_Diff)
+      apply (rule iffD2[OF disjoint_iff])
+      apply (rule allI)
+      apply (rule impI)
+      apply assumption
+     apply (rule arg_cong2[OF _ refl, of _ _ minus])
+     apply (unfold UN_simps)
+     apply (rule UN_cong)
+     apply assumption
+  (* copied from above *)
+     apply (rule UN_cong)
+    apply assumption
+    done
+  done
+
+
 
 end

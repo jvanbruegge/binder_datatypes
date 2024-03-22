98f4241043c58f678c6601d61b0c44122554c846
Dmitriy Traytel <traytel@di.ku.dk>
2024-03-22 16:51:22 +0100

    more

--- thys/No_Least_Support_Counterexample.thy
+++ thys/No_Least_Support_Counterexample.thy
@@ -1,30 +1,82 @@
 theory No_Least_Support_Counterexample
-  imports Generic_Barendregt_Enhanced_Rule_Induction "HOL-Library.Stream"
-    "Infinitary_Lambda_Calculus.Embed_var_ivar"
+  imports
+   "Prelim.FixedUncountableVars"
+   "Prelim.Prelim"
+   "Prelim.More_Stream"
 begin
 
-locale Nominal = Small dummy
-for dummy :: 'A
-+
-fixes (* 'T: term-like entities *)
-Tmap :: "('A \<Rightarrow> 'A) \<Rightarrow> 'T \<Rightarrow> 'T"
-assumes  
-Tmap_id: "Tmap id = id"
-and
-Tmap_comp: "\<And>\<sigma> \<tau>. ssbij \<sigma> \<Longrightarrow> ssbij \<tau> \<Longrightarrow> Tmap (\<sigma> o \<tau>) = Tmap \<sigma> o Tmap \<tau>"
-and
-small_support: "\<And>t. \<exists>A. small A \<and> (\<forall>\<sigma>. ssbij \<sigma> \<longrightarrow> (\<forall>a\<in>A. \<sigma> a = a) \<longrightarrow> Tmap \<sigma> t = t)"
-begin
+definition small :: "ivar set \<Rightarrow> bool" where 
+  "small A \<equiv> |A| <o |UNIV::ivar set|"
 
-definition supports where
-  "supports A t = (\<forall>\<sigma>. ssbij \<sigma> \<longrightarrow> (\<forall>a\<in>A. \<sigma> a = a) \<longrightarrow> Tmap \<sigma> t = t)"
+definition ssbij :: "(ivar \<Rightarrow> ivar) \<Rightarrow> bool" (* small-support bijections *) where 
+  "ssbij \<sigma> \<equiv> bij \<sigma> \<and> |supp \<sigma>| <o |UNIV::ivar set|"
 
+lemma small_Un: "small A \<Longrightarrow> small B \<Longrightarrow> small (A \<union> B)"
+  using countable_iff_le_card_ivar small_def by auto
 lemma small_Diff: "small A \<Longrightarrow> small (A - B)"
-  by (meson Diff_subset card_of_subset_bound small_def)
+  using countable_iff_le_card_ivar small_def by auto
 lemma small_Int: "small A \<Longrightarrow> small (A \<inter> B)"
   by (metis Diff_Diff_Int small_Diff)
 
-end
+lemma exists_subset_compl:
+  assumes "Cinfinite r" "r \<le>o |UNIV::'b set|" "|U \<union> S::'b set| <o r"
+  shows "\<exists>B. U \<inter> B = {} \<and> B \<inter> S = {} \<and> |U| =o |B|"
+proof -
+  have 1: "|U| <o r" using assms(3) using card_of_Un1 ordLeq_ordLess_trans by blast
+  have "|-(U \<union> S)| =o |UNIV::'b set|"
+    using card_of_Un_diff_infinite[OF
+        cinfinite_imp_infinite[OF cinfinite_mono[OF assms(2) conjunct1[OF assms(1)]]]
+        ordLess_ordLeq_trans[OF assms(3,2)]
+    ]
+    by (simp add: Compl_eq_Diff_UNIV)
+  then have "|U| <o |-(U \<union> S)|" using ordLess_ordLeq_trans[OF 1 assms(2)] ordIso_symmetric ordLess_ordIso_trans by fast
+  then obtain B where 1: "B \<subseteq> -(U \<union> S)" "|U| =o |B|"
+    by (meson internalize_card_of_ordLeq2 ordLess_imp_ordLeq)
+  then have "U \<inter> B = {}" "B \<inter> S = {}" by blast+
+  then show ?thesis using 1 by blast
+qed
+
+lemma small_ssbij: 
+assumes s: "small A" "small B" "small A'" "A \<inter> A' = {}"
+shows "\<exists>\<sigma>. ssbij \<sigma> \<and> \<sigma> ` A \<inter> B = {} \<and> (\<forall>a\<in>A'. \<sigma> a = a)"
+proof-
+  obtain D where D: "D \<inter> B = {}" "D \<inter> A = {}" "D \<inter> A' = {}" and DA: "|D| =o |A|"
+    using exists_subset_compl[of _ A "A' \<union> B"]
+    by (smt (verit, best) Field_card_of Int_Un_distrib Int_commute Un_empty card_of_Card_order
+      card_of_UNIV cinfinite_def countable_Un_iff countable_iff_le_card_ivar infinite_ivar
+      ordIso_symmetric s(1) s(2) s(3) small_def)
+
+  then obtain u where u: "bij_betw u A D"  
+  using card_of_ordIso ordIso_symmetric by blast
+  hence u: "u ` A = D" "\<And>a b. a \<in> A \<Longrightarrow> b \<in> A \<Longrightarrow> u a = u b \<longleftrightarrow> a = b"
+  unfolding bij_betw_def inj_on_def by auto
+
+  let ?iu = "inv_into A u" 
+  have iu: "bij_betw ?iu D A"
+  using u by (metis bij_betw_def bij_betw_inv_into inj_on_def)
+  hence iu: "?iu ` D = A" "\<And>a b. a \<in> D \<Longrightarrow> b \<in> D \<Longrightarrow> ?iu a = ?iu b \<longleftrightarrow> a = b"
+  unfolding bij_betw_def inj_on_def by auto
+
+  define v where "v \<equiv> \<lambda>a. if a \<in> A then u a else if a \<in> D then ?iu a else a"
+  have v[simp]: "\<And>a. a \<in> A \<Longrightarrow> v a = u a" "\<And>a. a \<in> D \<Longrightarrow> v a = ?iu a"
+  "\<And>a. a \<notin> A \<and> a \<notin> D \<Longrightarrow> v a = a"
+  using D(2) unfolding v_def by auto
+
+  have cas: "\<And>a. a \<in> A \<or> a \<in> D \<or> (a \<notin> A \<and> a \<notin> D)" by auto
+
+  have bv: "bij v"
+  unfolding bij_def inj_def image_def apply auto 
+  apply (metis (mono_tags, lifting) D(2) Int_emptyD f_inv_into_f imageI iu(1) u(1) u(2) v_def)
+  by (metis f_inv_into_f inv_into_into iu(1) u(1) v(2) v_def) 
+
+  have sv: "supp v \<subseteq> A \<union> D" unfolding supp_def using v(3) by blast
+
+  show ?thesis apply(rule exI[of _ v], intro conjI)
+    subgoal using bv sv s(1) unfolding ssbij_def small_def
+      by (meson DA card_of_Un_ordLess_infinite card_of_mono1 infinite_ivar ordIso_ordLess_trans ordLeq_ordLess_trans)
+    subgoal using D(1) u(1) by auto
+    subgoal using sv D(3) s(4) unfolding supp_def by auto . 
+qed
 
 definition "eq_ae s t = finite {i. snth s i \<noteq> snth t i}"
 
@@ -76,36 +128,40 @@ lift_definition map_item :: "('a \<Rightarrow> 'b) \<Rightarrow> 'a item \<Right
     done
   done
 
-interpretation Small "undefined :: ivar"
-  by standard (simp_all add: regularCard_ivar infinite)
+definition supports where
+  "supports A t = (\<forall>\<sigma>. ssbij \<sigma> \<longrightarrow> (\<forall>a\<in>A. \<sigma> a = a) \<longrightarrow> map_item \<sigma> t = t)"
 
-interpretation Nominal "undefined :: ivar" map_item
-  apply standard
-  subgoal
-    by (rule ext, transfer) (auto simp: eq_ae_refl)
-  subgoal for \<sigma> \<tau>
-    by (rule ext, transfer) (auto simp: eq_ae_refl stream.map_comp)
-  subgoal for t
-    apply transfer
-    subgoal for s
-      apply (rule exI[of _ "sset s"])
-      apply safe
-      subgoal
-        unfolding small_def
-        using countable_card_ivar countable_sset by blast
-      subgoal
-        by (auto simp: eq_ae_refl stream.map_ident cong: stream.map_cong)
-      done
+(* next 3 lemmas show that item is a nominal set *)
+lemma map_item_id: "map_item id = id"
+ by (rule ext, transfer) (auto simp: eq_ae_refl)
+lemma map_item_comp: "ssbij \<sigma> \<Longrightarrow> ssbij \<tau> \<Longrightarrow> map_item (\<sigma> o \<tau>) = map_item \<sigma> o map_item \<tau>"
+  by (rule ext, transfer) (auto simp: eq_ae_refl stream.map_comp)
+lemma small_support: "\<exists>A. small A \<and> supports A t"
+  unfolding supports_def
+  apply transfer
+  subgoal for s
+    apply (rule exI[of _ "sset s"])
+    apply safe
+    subgoal
+      unfolding small_def
+      using countable_card_ivar countable_sset by blast
+    subgoal
+      by (auto simp: eq_ae_refl stream.map_ident cong: stream.map_cong)
     done
   done
 
-lift_definition inats :: "ivar item" is "smap iVariable nats" .
+definition "ivar = (SOME f :: nat \<Rightarrow> ivar. inj f)"
 
-lemma small_from: "small (iVariable ` {n ..})"
+lemma ivar_inj[simp]: "ivar x = ivar y \<longleftrightarrow> x = y"
+  by (metis infinite_countable_subset infinite_ivar injD ivar_def someI_ex)
+
+lift_definition inats :: "ivar item" is "smap ivar nats" .
+
+lemma small_from: "small (ivar ` {n ..})"
   unfolding small_def
   by (rule countable_card_ivar) auto
 
-lemma supports_inats: "supports (iVariable ` {n ..}) inats"
+lemma supports_inats: "supports (ivar ` {n ..}) inats"
   unfolding supports_def
   apply transfer
   apply safe
@@ -113,21 +169,22 @@ lemma supports_inats: "supports (iVariable ` {n ..}) inats"
     by (auto simp: stream.map_comp eq_ae_suffix intro!: exI[of _ n] stream.map_cong)
   done
 
+(* assuming the existence of a minimal support set for any item we derive a contradiction *)
 context
-  assumes minimal: "\<exists>A. small A \<and> supports A t \<and> (\<forall>B. small B \<longrightarrow> supports B t \<longrightarrow> A \<subseteq> B)"
+  assumes minimal: "\<forall>t. \<exists>A. small A \<and> supports A t \<and> (\<forall>B. small B \<longrightarrow> supports B t \<longrightarrow> A \<subseteq> B)"
 begin
 
 lemma False
 proof -
   from minimal obtain A where "small A" "supports A inats" "\<forall>B. small B \<longrightarrow> supports B inats \<longrightarrow> A \<subseteq> B"
     by blast
-  then have *: "A \<subseteq> iVariable ` {n..}" for n
+  then have *: "A \<subseteq> ivar ` {n..}" for n
     using small_from[of n] supports_inats[of n]
     by auto
-  have "iVariable x \<notin> A" for x
+  have "ivar x \<notin> A" for x
     using *[of "Suc x"] by auto
-  with small_ssbij[of "range iVariable" "range iVariable" A] small_from[of 0] \<open>small A\<close> obtain \<sigma> where
-    "ssbij \<sigma>" "\<sigma> ` range iVariable \<inter> range iVariable = {}" "\<forall>a\<in>A. \<sigma> a = a"
+  with small_ssbij[of "range ivar" "range ivar" A] small_from[of 0] \<open>small A\<close> obtain \<sigma> where
+    "ssbij \<sigma>" "\<sigma> ` range ivar \<inter> range ivar = {}" "\<forall>a\<in>A. \<sigma> a = a"
     by auto
   then show False
     using \<open>supports A inats\<close>[unfolded supports_def, rule_format, where \<sigma> = \<sigma>]

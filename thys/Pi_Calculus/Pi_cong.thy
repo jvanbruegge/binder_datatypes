d595bad446eb84fe0c02e054850966b18cee9223
Jan van Brügge <supermanitu@gmail.com>
2024-03-23 16:25:14 +0000

    Add reduction semantics for congruent pi calculus

--- thys/Pi_Calculus/Pi_cong.thy
+++ thys/Pi_Calculus/Pi_cong.thy
@@ -157,4 +157,113 @@ shows "\<phi> P Q p"
     done
   done
 
+(* Also inferring equivariance from the general infrastructure: *)
+corollary rrename_cong:
+assumes f: "bij f" "|supp f| <o |UNIV::var set|" 
+and r: "P \<equiv>\<^sub>\<pi> Q" 
+shows "rrename f P \<equiv>\<^sub>\<pi> rrename f Q"
+using assms unfolding cong_I using Cong.I_equiv[of "(P,Q)" f]
+unfolding Tmap_def ssbij_def by auto
+
+inductive trans :: "trm \<Rightarrow> trm \<Rightarrow> bool" (infix "(\<rightarrow>)" 30) where
+  "Par (Out x z P) (Inp x y Q) \<rightarrow> Par P (usub Q z y)"
+| "P \<rightarrow> Q \<Longrightarrow> Par P R \<rightarrow> Par P Q"
+| "P \<rightarrow> Q \<Longrightarrow> Res x P \<rightarrow> Res x Q"
+| "P \<equiv>\<^sub>\<pi> P' \<Longrightarrow> P' \<rightarrow> Q' \<Longrightarrow> Q' \<equiv>\<^sub>\<pi> Q \<Longrightarrow> P \<rightarrow> Q"
+
+thm trans_def
+
+definition G_trans :: "var set \<Rightarrow> (T \<Rightarrow> bool) \<Rightarrow> T \<Rightarrow> bool" where
+  "G_trans \<equiv> \<lambda>B p t.
+    (\<exists>x z P y Q. B = {y} \<and> fst t = Par (Out x z P) (Inp x y Q) \<and> snd t = Par P (usub Q z y)) \<or>
+    (\<exists>P Q R. B = {} \<and> fst t = Par P R \<and> snd t = Par P Q \<and> p (P, Q)) \<or>
+    (\<exists>P Q x. B = {x} \<and> fst t = Res x P \<and> snd t = Res x Q \<and> p (P, Q)) \<or>
+    (\<exists>P P' Q' Q. B = {} \<and> fst t = P \<and> snd t = Q \<and> (P \<equiv>\<^sub>\<pi> P') \<and> p (P', Q') \<and> (Q' \<equiv>\<^sub>\<pi> Q))"
+
+lemma GG_trans_mono: "R \<le> R' \<Longrightarrow> G_trans v R t \<Longrightarrow> G_trans v R' t"
+  unfolding G_trans_def by blast
+
+lemma GG_trans_equiv: "ssbij \<sigma> \<Longrightarrow> small B \<Longrightarrow> G_trans B R t \<Longrightarrow> G_trans (image \<sigma> B) (\<lambda>t'. R (Tmap (inv \<sigma>) t')) (Tmap \<sigma> t)"
+  unfolding G_trans_def
+  apply (elim disj_forward exE; cases t)
+  apply  (auto simp: Tmap_def ssbij_def term.rrename_comps
+         | ((rule exI[of _ "\<sigma> _"] exI)+, (rule conjI)?, rule refl)
+         | ((rule exI[of _ "\<sigma> _"])+; auto))+
+  by (metis Pi_cong.rrename_cong bij_imp_inv' term.rrename_bijs term.rrename_inv_simps)
+
+lemma G_trans_refresh: 
+  assumes "(\<forall>\<sigma> t. ssbij \<sigma> \<and> R t \<longrightarrow> R (Tmap \<sigma> t))"
+  shows "small B \<Longrightarrow> G_trans B R t \<Longrightarrow> \<exists>C. small C \<and> C \<inter> Tfvars t = {} \<and> G_trans C R t"
+unfolding G_trans_def
+(**)ssbij_def conj_assoc[symmetric]
+  unfolding ex_push_inwards conj_disj_distribL ex_disj_distrib ex_simps(1,2)[symmetric]
+    ex_comm[where P = P for P :: "_ set \<Rightarrow> _ \<Rightarrow> _"]
+  apply (elim disj_forward exE; simp; clarsimp)
+  apply (cases t; auto simp only: fst_conv snd_conv Tfvars.simps term.set)
+  subgoal for x z P y Q
+    apply (rule exE[OF exists_fresh[of "[x, y, z]" "[(P, Q)]"]])
+    subgoal for w
+      apply (rule exI[of _ w])
+      apply simp
+      by (meson Inp_refresh usub_refresh)
+    done
+  apply ((((rule exI conjI)+)?, (assumption | rule Inp_refresh Res_refresh usub_refresh arg_cong2[where f=Cmt, OF refl])
+        | (erule (1) R_forw_subst[of R, OF _ assms[unfolded Tmap_def, simplified, rule_format, OF conjI[OF ssbij_swap]]]; simp?)
+        | (cases t; auto simp only: fst_conv snd_conv Tfvars.simps term.set))+)
+  done
+
+interpretation Trans: Induct where dummy = "undefined :: var" and 
+Tmap = Tmap and Tfvars = Tfvars and G = G_trans
+apply standard 
+  using GG_trans_mono GG_trans_equiv G_trans_refresh by auto
+
+lemma trans_I: "trans t1 t2 = Trans.I (t1,t2)"
+unfolding trans_def Trans.I_def lfp_curry2 apply(rule arg_cong2[of _ _ _ _ lfp], simp_all)
+  unfolding fun_eq_iff G_trans_def apply clarify
+subgoal for R PP QQ apply(rule iffI)
+  subgoal apply(elim disjE exE)
+    subgoal by fastforce
+    subgoal by force
+    subgoal by force
+    subgoal by force
+    done
+  subgoal apply(elim disjE exE)
+    subgoal by fastforce
+    done
+  done
+  done
+
+thm trans.induct[of P Q \<phi>, no_vars]
+
+corollary strong_induct_trans[consumes 2]:
+  assumes par: "\<And>p. small (Pfvars p)"
+  and tr: "trans P Q"
+  and rules:
+  "\<And>x z P y Q p. y \<notin> Pfvars p \<Longrightarrow> \<phi> (Par (Out x z P) (Inp x y Q)) (Par P (usub Q z y)) p"
+  "\<And>P Q R p. P \<rightarrow> Q \<Longrightarrow> (\<And>p. \<phi> P Q p) \<Longrightarrow> \<phi> (Par P R) (Par P Q) p"
+  "\<And>P Q x p. x \<notin> Pfvars p \<Longrightarrow> P \<rightarrow> Q \<Longrightarrow> (\<And>p. \<phi> P Q p) \<Longrightarrow> \<phi> (Res x P) (Res x Q) p"
+  "\<And>P P' Q' Q p. P \<equiv>\<^sub>\<pi> P' \<Longrightarrow> P' \<rightarrow> Q' \<Longrightarrow> (\<And>p. \<phi> P' Q' p) \<Longrightarrow> Q' \<equiv>\<^sub>\<pi> Q \<Longrightarrow> \<phi> P Q p"
+shows "\<phi> P Q p"
+apply(subgoal_tac "case (P,Q) of (P, Q) \<Rightarrow> \<phi> P Q p")
+  subgoal by simp
+  subgoal using par tr apply(elim Trans.strong_induct[where R = "\<lambda>p (P,Q). \<phi> P Q p"])
+    subgoal unfolding trans_I by simp
+    subgoal for p v t apply(subst (asm) G_trans_def) 
+      unfolding trans_I[symmetric] apply(elim disjE exE)
+      subgoal using rules(1) by auto
+      subgoal using rules(2) by auto
+      subgoal using rules(3) by auto
+      subgoal using rules(4) by auto
+      done
+    done
+  done
+
+(* Also inferring equivariance from the general infrastructure: *)
+corollary rrename_trans:
+assumes f: "bij f" "|supp f| <o |UNIV::var set|" 
+and r: "trans P Q" 
+shows "trans (rrename f P) (rrename f Q)"
+using assms unfolding trans_I using Trans.I_equiv[of "(P,Q)" f]
+unfolding Tmap_def ssbij_def by auto
+
 end

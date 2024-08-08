807edcdf96db984603568654c88ae05f84d7103c
Jan van Brügge <supermanitu@gmail.com>
2024-08-08 16:16:03 +0100

    Add bound free position to fixpoint operation

--- operations/Composition.thy
+++ operations/Composition.thy
@@ -11,6 +11,7 @@ val ctor_T1_Ts = [
   [@{typ unit}],
   [@{typ 'tyvar}],
   [@{typ 'rec}, @{typ 'rec2}],
+  [@{typ 'bvar}, @{typ "('bfree \<times> unit) list"}],
   [@{typ 'bvar}, @{typ 'brec}],
   [@{typ 'btyvar}, @{typ 'brec}],
   [@{typ 'a}]
@@ -40,7 +41,7 @@ declare [[mrbnf_internals]]
 local_setup \<open>fn lthy =>
 let
   val Xs = map dest_TFree [@{typ 'a}, @{typ 'b}]
-  val resBs = map dest_TFree [@{typ 'var}, @{typ 'tyvar}, @{typ 'bvar}, @{typ 'btyvar}, @{typ 'rec}, @{typ 'brec}, @{typ 'rec2}, @{typ 'brec2}]
+  val resBs = map dest_TFree [@{typ 'var}, @{typ 'tyvar}, @{typ 'bvar}, @{typ 'btyvar}, @{typ 'bfree}, @{typ 'rec}, @{typ 'brec}, @{typ 'rec2}, @{typ 'brec2}]
 
   fun flatten_tyargs Ass = subtract (op =) Xs (filter (fn T => exists (fn Ts => member (op =) Ts T) Ass) resBs) @ Xs;
   val qualify1 = Binding.prefix_name (name1 ^ "_pre_")
@@ -51,14 +52,14 @@ let
   val ((mrbnf1, tys1), (accum, lthy)) = MRBNF_Comp.mrbnf_of_typ true MRBNF_Def.Smart_Inline qualify1 flatten_tyargs Xs []
     [(dest_TFree @{typ 'var}, MRBNF_Def.Free_Var), (dest_TFree @{typ 'bvar}, MRBNF_Def.Bound_Var),
       (dest_TFree @{typ 'tyvar}, MRBNF_Def.Free_Var), (dest_TFree @{typ 'btyvar}, MRBNF_Def.Bound_Var),
-      (dest_TFree @{typ 'a}, MRBNF_Def.Free_Var)
+      (dest_TFree @{typ 'a}, MRBNF_Def.Free_Var), (dest_TFree @{typ 'bfree}, MRBNF_Def.Free_Var)
     ] T1
     (accum, lthy)
   val _ = @{print} "comp1"
   val ((mrbnf2, tys2), (accum, lthy)) = MRBNF_Comp.mrbnf_of_typ true MRBNF_Def.Smart_Inline qualify2 flatten_tyargs Xs []
     [(dest_TFree @{typ 'var}, MRBNF_Def.Free_Var), (dest_TFree @{typ 'bvar}, MRBNF_Def.Bound_Var),
       (dest_TFree @{typ 'tyvar}, MRBNF_Def.Free_Var), (dest_TFree @{typ 'btyvar}, MRBNF_Def.Bound_Var),
-      (dest_TFree @{typ 'a}, MRBNF_Def.Free_Var)
+      (dest_TFree @{typ 'a}, MRBNF_Def.Free_Var), (dest_TFree @{typ 'bfree}, MRBNF_Def.Free_Var)
     ] T2
     (accum, lthy);
   val _ = @{print} "comp2"
@@ -83,7 +84,7 @@ let
 in lthy end
 \<close>
 print_theorems
-print_bnfs
+print_mrbnfs
 
 declare [[quick_and_dirty=false]]
 

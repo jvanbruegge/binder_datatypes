a47ab85d42741a234b87047239fe5fd39ac5286c
Jan van Brügge <supermanitu@gmail.com>
2025-04-01 11:18:52 +0100

    Simplify id_on set in quasi-inject theorem more
    ISSUES FIXED: #69

--- tests/Regression_Tests.thy
+++ tests/Regression_Tests.thy
@@ -1,10 +1,17 @@
 theory Regression_Tests
-  imports "Binders.MRBNF_Recursor"
+  imports "Binders.MRBNF_Recursor" "../thys/LetRec/DAList_MRBNF"
 begin
 
 (* types have the GitHub issue number as postfix *)
 
-binder_datatype 'a trm_68 = Var 'a | Abs x::'a t::"'a trm_68" binds x in t
+binder_datatype 'a trm_68 =
+  Var 'a
+| Abs x::'a t::"'a trm_68" binds x in t
   
+binder_datatype 'a LLC_69 =
+  Var 'a
+  | App "'a LLC_69" "'a LLC_69"
+  | Abs x::'a t::"'a LLC_69" binds x in t
+  | Let "(x::'a, t::'a LLC_69) alist" u::"'a LLC_69" binds x in t u
 
 end

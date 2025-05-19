7a44268e89d0f1a742a5c98e1a9abc0ca1019114
Felix Krayer <felix.krayer@tum.de>
2025-05-19 11:05:09 +0200

    change lifting defintions to definitions and adjust proofs (remove transfer)

--- operations/Linearize.thy
+++ operations/Linearize.thy
@@ -30,6 +30,17 @@ mrbnf "('a :: var, 'b :: var, 'c, 'd) F"
   var_class: var
   sorry
 
+ML \<open>map (fn x => x+1) [1, 2, 3] \<close>
+
+ML \<open>@{term "map (\<lambda>x. x+1) [(1::int), 2, 3]"} \<close>
+ML \<open>Const ("List.list.Nil", @{typ"int list"})\<close>
+
+ML \<open>@{thm refl}\<close>
+
+print_mrbnfs
+declare [[ML_print_depth=10000]]
+ML \<open>MRBNF_Def.mrbnf_of @{context} @{type_name F} |> the \<close>
+
 axiomatization where
 (* The next property assumes preservation of pullbacks on the third position. 
    NB: All MRBNFs already preserve _weak_ pullbacks, i.e., they satisfy the following property 
@@ -187,26 +198,28 @@ typedef ('a1::var,'a2::var,'a3::var,'a4) F' = "{x :: ('a1,'a2,'a3,'a4) F. nonrep
 
 setup_lifting type_definition_F'
 
-lift_definition set1_F' :: "('a1::var,'a2::var,'a3::var,'a4) F' \<Rightarrow> 'a1 set" is set1_F .
-lift_definition set2_F' :: "('a1::var,'a2::var,'a3::var,'a4) F' \<Rightarrow> 'a2 set" is set2_F .
-lift_definition set3_F' :: "('a1::var,'a2::var,'a3::var,'a4) F' \<Rightarrow> 'a3 set" is set3_F .
-lift_definition set4_F' :: "('a1::var,'a2::var,'a3::var,'a4) F' \<Rightarrow> 'a4 set" is set4_F .
+definition set1_F' :: "('a1::var,'a2::var,'a3::var,'a4) F' \<Rightarrow> 'a1 set" where "set1_F' = set1_F o Rep_F'"
+definition set2_F' :: "('a1::var,'a2::var,'a3::var,'a4) F' \<Rightarrow> 'a2 set" where "set2_F' = set2_F o Rep_F'"
+definition set3_F' :: "('a1::var,'a2::var,'a3::var,'a4) F' \<Rightarrow> 'a3 set" where "set3_F' = set3_F o Rep_F'"
+definition set4_F' :: "('a1::var,'a2::var,'a3::var,'a4) F' \<Rightarrow> 'a4 set" where "set4_F' = set4_F o Rep_F'"
 
 definition asSS :: "('a \<Rightarrow> 'a) \<Rightarrow> 'a \<Rightarrow> 'a" where
   "asSS f \<equiv> if |supp f| <o |UNIV :: 'a set| then f else id"
 
-lift_definition map_F' :: "('a1::var \<Rightarrow> 'a1) \<Rightarrow> ('a2::var \<Rightarrow> 'a2) \<Rightarrow> ('a3::var \<Rightarrow> 'a3) \<Rightarrow> ('a4 \<Rightarrow> 'a4') 
+definition map_F' :: "('a1::var \<Rightarrow> 'a1) \<Rightarrow> ('a2::var \<Rightarrow> 'a2) \<Rightarrow> ('a3::var \<Rightarrow> 'a3) \<Rightarrow> ('a4 \<Rightarrow> 'a4') 
   \<Rightarrow> ('a1,'a2,'a3,'a4) F' \<Rightarrow> ('a1,'a2,'a3,'a4') F'"
-  is "\<lambda>v u f g. map_F (asSS v) (asSS (asBij u)) (asBij f) g" 
-  unfolding asBij_def asSS_def by (auto simp: supp_id_bound intro: nonrep2_mapF_bij_2)
+  where "map_F' v u f g = Abs_F' o map_F (asSS v) (asSS (asBij u)) (asBij f) g o Rep_F'"
 
-lift_definition rrel_F' :: "('a4 \<Rightarrow> 'a4' \<Rightarrow> bool) \<Rightarrow> ('a1::var,'a2::var,'a3::var,'a4) F' \<Rightarrow> ('a1,'a2,'a3,'a4') F' \<Rightarrow> bool"
+definition rrel_F' :: "('a4 \<Rightarrow> 'a4' \<Rightarrow> bool) \<Rightarrow> ('a1::var,'a2::var,'a3::var,'a4) F' \<Rightarrow> ('a1,'a2,'a3,'a4') F' \<Rightarrow> bool"
+  where "rrel_F' R x x' = rrel_F (=) R (Rep_F' x) (Rep_F' x')"
+
+lift_definition rrel_F'_orig :: "('a4 \<Rightarrow> 'a4' \<Rightarrow> bool) \<Rightarrow> ('a1::var,'a2::var,'a3::var,'a4) F' \<Rightarrow> ('a1,'a2,'a3,'a4') F' \<Rightarrow> bool"
   is "rrel_F (=)" .
 
 (* Verifying the axioms of a MRBNF for F':  *)
 
 lemma F'_map_id: "map_F' id id id id = id"
-  by (rule ext, transfer) (auto simp: F.map_id asSS_def)
+  by (rule ext) (simp add: F.map_id asSS_def map_F'_def Rep_F'_inverse)
 
 lemma F'_map_comp1_:
   fixes u1 v1 :: "'a1::var \<Rightarrow> 'a1"
@@ -216,8 +229,9 @@ lemma F'_map_comp1_:
   assumes "bij u2" "|supp u2| <o |UNIV :: 'a2 set|" "bij v2" "|supp v2| <o |UNIV :: 'a2 set|"
   assumes "bij u3" "|supp u3| <o |UNIV :: 'a3 set|" "bij v3" "|supp v3| <o |UNIV :: 'a3 set|"
   shows "map_F' (v1 o u1) (v2 o u2) (v3 o u3) (g o f) = map_F' v1 v2 v3 g o map_F' u1 u2 u3 f"
-  using assms by (intro ext, transfer)
-    (auto simp: F.map_comp assms asBij_def asSS_def supp_comp_bound supp_id_bound infinite_UNIV)
+  using assms by (intro ext)
+    (simp add: F.map_comp assms asBij_def asSS_def supp_comp_bound supp_id_bound infinite_UNIV
+      map_F'_def nonrep2_mapF_bij_2[OF assms(1,3,4,7)] Rep_F'[simplified] Abs_F'_inverse)
 
 lemma F'_set1_map_:
   fixes u1 :: "'a1::var \<Rightarrow> 'a1"
@@ -227,7 +241,8 @@ lemma F'_set1_map_:
   assumes "bij u2" "|supp u2| <o |UNIV :: 'a2 set|"
   assumes "bij u3" "|supp u3| <o |UNIV :: 'a3 set|"
   shows "set1_F' (map_F' u1 u2 u3 f b) = u1 ` set1_F' b"
-  using assms by transfer (auto simp: F.set_map asSS_def)
+  using assms by (simp add: F.set_map asSS_def set1_F'_def map_F'_def
+      nonrep2_mapF_bij_2[OF assms(1-4)] Abs_F'_inverse Rep_F'[simplified])
 
 lemma F'_set2_map_:
   fixes u1 :: "'a1::var \<Rightarrow> 'a1"
@@ -237,7 +252,8 @@ lemma F'_set2_map_:
   assumes "bij u2" "|supp u2| <o |UNIV :: 'a2 set|"
   assumes "bij u3" "|supp u3| <o |UNIV :: 'a3 set|"
   shows "set2_F' (map_F' u1 u2 u3 f b) = u2 ` set2_F' b"
-  using assms by transfer (auto simp: F.set_map asSS_def)
+  using assms by (simp add: F.set_map asSS_def set2_F'_def map_F'_def
+      nonrep2_mapF_bij_2[OF assms(1-4)] Abs_F'_inverse Rep_F'[simplified])
 
 lemma F'_set3_map_:
   fixes u1 :: "'a1::var \<Rightarrow> 'a1"
@@ -247,7 +263,8 @@ lemma F'_set3_map_:
   assumes "bij u2" "|supp u2| <o |UNIV :: 'a2 set|"
   assumes "bij u3" "|supp u3| <o |UNIV :: 'a3 set|"
   shows "set3_F' (map_F' u1 u2 u3 f b) = u3 ` set3_F' b"
-  using assms by transfer (auto simp: F.set_map asSS_def)
+  using assms by (simp add: F.set_map asSS_def set3_F'_def map_F'_def
+      nonrep2_mapF_bij_2[OF assms(1-4)] Abs_F'_inverse Rep_F'[simplified])
 
 lemma F'_set4_map_:
   fixes u1 :: "'a1::var \<Rightarrow> 'a1"
@@ -257,7 +274,8 @@ lemma F'_set4_map_:
   assumes "bij u2" "|supp u2| <o |UNIV :: 'a2 set|"
   assumes "bij u3" "|supp u3| <o |UNIV :: 'a3 set|"
   shows "set4_F' (map_F' u1 u2 u3 f b) = f ` set4_F' b"
-  using assms by transfer (auto simp: F.set_map asSS_def)
+  using assms by (simp add: F.set_map asSS_def set4_F'_def map_F'_def
+      nonrep2_mapF_bij_2[OF assms(1-4)] Abs_F'_inverse Rep_F'[simplified])
 
 lemma F'_map_cong_[fundef_cong]:
   fixes u1 v1 :: "'a1::var \<Rightarrow> 'a1"
@@ -271,30 +289,41 @@ lemma F'_map_cong_[fundef_cong]:
     and "\<forall> a \<in> set3_F' x. u3 a = v3 a"
     and "\<forall> a \<in> set4_F' x. f a = g a"
   shows "map_F' u1 u2 u3 f x = map_F' v1 v2 v3 g x"
-  using assms by transfer (auto intro: F.map_cong simp: asSS_def)
+  using assms
+  unfolding map_F'_def set1_F'_def set2_F'_def set3_F'_def set4_F'_def asSS_def
+  by (simp add: F.map_cong[of u1 u2 v1 v2])
+
+thm F.map_cong[of u1 u2 v1 v2 "(Rep_F' x)" "(Rep_F' x)" u3 v3 f g]
 
 lemma F'_set1_bd: "\<And>b. |set1_F' b| <o natLeq"
-  by transfer (simp add: F.set_bd)
+  unfolding set1_F'_def
+  by (subst o_apply) (rule F.set_bd)
 lemma F'_set2_bd: "\<And>b. |set2_F' b| <o natLeq"
-  by transfer (simp add: F.set_bd)
+  unfolding set2_F'_def
+  by (subst o_apply) (rule F.set_bd)
 lemma F'_set3_bd: "\<And>b. |set3_F' b| <o natLeq"
-  by transfer (simp add: F.set_bd)
+  unfolding set3_F'_def
+  by (subst o_apply) (rule F.set_bd)
 lemma F'_set4_bd: "\<And>b. |set4_F' b| <o natLeq"
-  by transfer (simp add: F.set_bd)
+  unfolding set4_F'_def
+  by (subst o_apply) (rule F.set_bd)
 
 lemma F'_rel_comp_leq_: "rrel_F' Q OO rrel_F' R \<le> rrel_F' (Q OO R)"
-  apply (intro predicate2I, transfer)
-  apply (auto simp: F.rel_compp[of "(=)", simplified])
+  apply (rule predicate2I)
+  apply (auto simp: rrel_F'_def F.rel_compp[of "(=)", simplified])
   done
+
 
 lemma rrel_F_map_F3:
   fixes x :: "('a :: var,'b :: var,'c,'d) F"
   shows "rrel_F (Grp (f :: 'c \<Rightarrow> 'c)) R x y = rrel_F (=) R (map_F id id f id x) y"
-  unfolding F.rel_map
-  by (auto simp: Grp_def elim!: F.rel_mono_strong)
+  unfolding F.rel_map Grp_def
+  apply (rule iffI)
+  apply (erule F.rel_mono_strong; simp)+
+  done
 
 lemma asSS: "|supp u| <o |UNIV :: 'a set| \<Longrightarrow> asSS (u :: 'a \<Rightarrow> 'a) = u"
-  unfolding asSS_def by auto
+  unfolding asSS_def by simp
 
 lemma F'_in_rel:
   fixes u1 :: "'a1::var \<Rightarrow> 'a1"
@@ -306,27 +335,43 @@ lemma F'_in_rel:
   shows "rrel_F' R (map_F' u1 u2 u3 id x) y =
     (\<exists>z. set4_F' z \<subseteq> {(x, y). R x y} \<and> map_F' id id id fst z = x \<and> map_F' u1 u2 u3 snd z = y)"
   using assms
-  apply (transfer fixing: u1 u2 u3 R)
-  apply (auto simp: F.rel_map asSS supp_id_bound
-    trans[OF rrel_F_map_F3[of u3, symmetric] F.in_rel[of u1 u2],
+  unfolding rrel_F'_def set4_F'_def map_F'_def
+  apply (simp add: asSS Abs_F'_inverse nonrep2_mapF_bij_2[OF assms(1,2,3,4)] Rep_F'[simplified])
+  apply (simp add: Rep_F'[simplified] asSS F.rel_map supp_id_bound)
+  apply (simp add: trans[OF rrel_F_map_F3[of u3, symmetric] F.in_rel[of u1 u2],
     simplified F.map_comp u1 u2 u3 supp_id_bound bij_id o_id True_implies_equals id_o])
+  apply (auto)
+  unfolding Grp_def
   subgoal for z
-    apply (rule exI[of _ "map_F id id fst id z"])
-    apply (auto simp: F.set_map supp_id_bound F.map_comp Grp_def
-      intro!: F.map_cong nonrep2_map_F_rev[OF bij_id supp_id_bound, of fst])
-    done
+    apply (rule exI[of _ "Abs_F' (map_F id id fst id z)"])
+    apply (simp add: supp_id_bound F.map_comp[of id id id id id fst fst id]
+      nonrep2_map_F_rev[OF bij_id supp_id_bound, of fst] Abs_F'_inverse Rep_F'[simplified])
+    apply (intro conjI)
+    subgoal
+      by(simp add: F.set_map(4) supp_id_bound)
+    subgoal
+      by(rule Rep_F'_inverse)
+    subgoal
+      apply (auto simp: supp_id_bound F.map_comp intro!: F.map_cong)
+      sorry
+      done
   subgoal for z
-    apply (rule exI[of _ "map_F id id (\<lambda>x. (x, u3 x)) id z"])
-    apply (auto simp: F.set_map supp_id_bound F.map_comp Grp_def
-      intro!: F.map_cong)
+    apply (rule exI[of _ "map_F id id (\<lambda>x. (x, u3 x)) id (Rep_F' z)"])
+    apply (simp add: Abs_F'_inverse Rep_F'[simplified]
+        nonrep2_mapF_bij_2 supp_id)
+    apply (auto simp: F.set_map supp_id_bound F.map_comp intro: F.map_cong)
     done
   done
+
 
 lemma F'_strong:
   assumes "rrel_F' R x x'" 
     and "rrel_F' Q x x'"
   shows "rrel_F' (inf R Q) x x'" 
-  using assms apply transfer using F_strong unfolding mr_rel_F_def F.map_id by fastforce
+  using assms F_strong[of "(=)" _ _ _ "(=)"] 
+  by(simp add: rrel_F'_def mr_rel_F_def F.map_id)
+
+find_theorems "(\<And>x. _  = _)  \<Longrightarrow> _ = _ "
 
 mrbnf "('a::var, 'b::var, 'c::var, 'd) F'"
   map: map_F'
@@ -337,10 +382,10 @@ mrbnf "('a::var, 'b::var, 'c::var, 'd) F'"
   subgoal by (rule F'_map_id)
   subgoal by (rule F'_map_comp1_)
   subgoal by (rule F'_map_cong_; blast)
-  subgoal by (auto simp: F'_set1_map_)
-  subgoal by (auto simp: F'_set2_map_)
-  subgoal by (auto simp: F'_set3_map_)
-  subgoal by (auto simp: F'_set4_map_)
+  subgoal by (standard, simp add: F'_set1_map_)
+  subgoal by (standard, simp add: F'_set2_map_)
+  subgoal by (standard, simp add: F'_set3_map_)
+  subgoal by (standard, simp add: F'_set4_map_)
   subgoal by (rule infinite_regular_card_order_natLeq)
   subgoal by (rule F'_set1_bd)
   subgoal by (rule F'_set2_bd)

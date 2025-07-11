d1b7528b1906f1c78e5e280f4c796929a0fa9be8
Dmitriy Traytel <traytel@di.ku.dk>
2025-07-11 13:53:29 +0200

    more

--- thys/Substitution/Expression_Like_Birecursor.thy
+++ thys/Substitution/Expression_Like_Birecursor.thy
@@ -4,10 +4,8 @@ begin
 
 locale Bimodel = Nominal Pperm "PVrs :: 'p \<Rightarrow> 'a :: var set" + Expression Eperm "EVrs :: 'e \<Rightarrow> 'a set" Ector
   for Pperm PVrs Eperm EVrs Ector +
-  fixes  is_base :: "('a::var, 'a, 'e, 'e) G \<Rightarrow> bool"
-  and Ector_base :: "('a::var, 'a, 'e, 'e) G \<Rightarrow> 'p \<Rightarrow> 'e"
-  and Ector_step :: "('a::var, 'a, 'p \<Rightarrow> 'e, 'p \<Rightarrow> 'e) G \<Rightarrow> 'p \<Rightarrow> 'e"
-  assumes TODO: "\<exists>P. P is_base Ector_base Ector_step" (* TODO 1 here we need the assumptions on bimodels*)
+  fixes Ector' :: "('a::var, 'a, 'p \<Rightarrow> 'e, 'p \<Rightarrow> 'e) G \<Rightarrow> 'p \<Rightarrow> 'e"
+  assumes TODO: "\<exists>P. P Ector'" (* TODO 1 here we need the assumptions on bimodels*)
 
 (*
 TODO the below Birecursor locale needs to be interpreted for both datatypes and codatatypes
@@ -17,38 +15,32 @@ the existing interpretations in that theory that define substitution directly sh
 locale Birecursor = Expression Eperm "EVrs :: 'e \<Rightarrow> 'a :: var set" Ector
   for Eperm EVrs Ector +
   fixes Pdummy :: 'p
-  assumes rec: "\<forall>Pperm (PVrs :: 'p \<Rightarrow> 'a set) is_base Ector_base Ector_step.
-    Bimodel Pperm PVrs Eperm EVrs Ector is_base Ector_base Ector_step \<longrightarrow> (\<exists>rec.
-      ((\<forall>u p. is_base u \<longrightarrow> GVrs2 u \<inter> PVrs p = {} \<longrightarrow> rec (Ector u) p = Ector_base u p) \<and>
-       (\<forall>u p. \<not> is_base u \<longrightarrow> GVrs2 u \<inter> PVrs p = {} \<longrightarrow> rec (Ector u) p = Ector_step (Gmap rec rec u) p) \<and>
+  assumes rec: "\<forall>Pperm (PVrs :: 'p \<Rightarrow> 'a set) Ector'.
+    Bimodel Pperm PVrs Eperm EVrs Ector Ector' \<longrightarrow> (\<exists>rec.
+      ((\<forall>u p. GVrs2 u \<inter> PVrs p = {} \<longrightarrow> rec (Ector u) p = Ector' (Gmap rec rec u) p) \<and>
        (\<forall>e p \<sigma>. bij \<sigma> \<longrightarrow> |supp \<sigma>| <o |UNIV :: 'a set| \<longrightarrow> rec (Eperm \<sigma> e) p = Eperm \<sigma> (rec e (Pperm (inv \<sigma>) p))) \<and>
        (\<forall>e p. EVrs (rec e p) \<subseteq> PVrs p \<union> EVrs e)))"
 begin
 
-context fixes Pperm and PVrs :: "'p \<Rightarrow> 'a set" and is_base Ector_base Ector_step
-  assumes BM: "Bimodel Pperm PVrs Eperm EVrs Ector is_base Ector_base Ector_step"
+context fixes Pperm and PVrs :: "'p \<Rightarrow> 'a set" and Ector'
+  assumes BM: "Bimodel Pperm PVrs Eperm EVrs Ector Ector'"
 begin
 
 definition rec where
-  "rec = (SOME rec. ((\<forall>u p. is_base u \<longrightarrow> GVrs2 u \<inter> PVrs p = {} \<longrightarrow> rec (Ector u) p = Ector_base u p) \<and>
-       (\<forall>u p. \<not> is_base u \<longrightarrow> GVrs2 u \<inter> PVrs p = {} \<longrightarrow> rec (Ector u) p = Ector_step (Gmap rec rec u) p) \<and>
+  "rec = (SOME rec. ((\<forall>u p. GVrs2 u \<inter> PVrs p = {} \<longrightarrow> rec (Ector u) p = Ector' (Gmap rec rec u) p) \<and>
        (\<forall>e p \<sigma>. bij \<sigma> \<longrightarrow> |supp \<sigma>| <o |UNIV :: 'a set| \<longrightarrow> rec (Eperm \<sigma> e) p = Eperm \<sigma> (rec e (Pperm (inv \<sigma>) p))) \<and>
        (\<forall>e p. EVrs (rec e p) \<subseteq> PVrs p \<union> EVrs e)))"
 
-lemma rec_base: "is_base u \<Longrightarrow> GVrs2 u \<inter> PVrs p = {} \<Longrightarrow> rec (Ector u) p = Ector_base u p"
+lemma rec_Ector: "GVrs2 u \<inter> PVrs p = {} \<Longrightarrow> rec (Ector u) p = Ector' (Gmap rec rec u) p"
   using someI_ex[OF rec[rule_format, OF BM], THEN conjunct1] unfolding rec_def
   by blast
 
-lemma rec_Ector: "\<not> is_base u \<Longrightarrow> GVrs2 u \<inter> PVrs p = {} \<Longrightarrow> rec (Ector u) p = Ector_step (Gmap rec rec u) p"
+lemma rec_EPerm: "bij \<sigma> \<Longrightarrow> |supp \<sigma>| <o |UNIV :: 'a set| \<Longrightarrow> rec (Eperm \<sigma> e) p = Eperm \<sigma> (rec e (Pperm (inv \<sigma>) p))"
   using someI_ex[OF rec[rule_format, OF BM], THEN conjunct2, THEN conjunct1] unfolding rec_def
   by blast
 
-lemma rec_EPerm: "bij \<sigma> \<Longrightarrow> |supp \<sigma>| <o |UNIV :: 'a set| \<Longrightarrow> rec (Eperm \<sigma> e) p = Eperm \<sigma> (rec e (Pperm (inv \<sigma>) p))"
-  using someI_ex[OF rec[rule_format, OF BM], THEN conjunct2, THEN conjunct2, THEN conjunct1] unfolding rec_def
-  by blast
-
 lemma rec_EVrs: "EVrs (rec e p) \<subseteq> PVrs p \<union> EVrs e"
-  using someI_ex[OF rec[rule_format, OF BM], THEN conjunct2, THEN conjunct2, THEN conjunct2] unfolding rec_def
+  using someI_ex[OF rec[rule_format, OF BM], THEN conjunct2, THEN conjunct2] unfolding rec_def
   by blast
 
 end
@@ -93,10 +85,9 @@ lemma permute_\<rho>':
 
 definition "Esub_Pperm \<equiv> \<lambda>\<sigma> (\<delta>, \<rho>, \<rho>'). (\<sigma> o \<delta> o inv \<sigma>, Eperm \<sigma> o \<rho> o inv \<sigma>, Eperm \<sigma> o \<rho>' o inv \<sigma>)"
 definition "Esub_PVrs \<equiv> \<lambda>(\<delta>, \<rho>, \<rho>'). imsupp \<delta> \<union> IImsupp' (Ector \<circ> \<eta>) EVrs \<rho> \<union> IImsupp' (Ector \<circ> \<eta>') EVrs \<rho>'"
-definition "Esub_is_base \<equiv> \<lambda>u. \<exists>a. u = \<eta> a \<or> u = \<eta>' a"
-definition "Esub_Ector_base \<equiv> \<lambda>u (\<delta>, \<rho>, \<rho>'). if (\<exists>a. u = \<eta> a) then \<rho> (SOME a. u = \<eta> a) else \<rho>' (SOME a. u = \<eta>' a)"
-definition "Esub_Ector_step \<equiv> \<lambda>u (\<delta>, \<rho>, \<rho>'). Ector (Gsub \<delta> id (Gmap (\<lambda>rec. rec (\<delta>, \<rho>, \<rho>')) (\<lambda>rec. rec (\<delta>, \<rho>, \<rho>')) u))"
-lemmas Esub_defs = Esub_Pperm_def Esub_PVrs_def Esub_is_base_def Esub_Ector_base_def Esub_Ector_step_def
+definition "Esub_Ector' \<equiv> \<lambda>u (\<delta>, \<rho>, \<rho>'). if \<exists>a. u = \<eta> a then \<rho> (SOME a. u = \<eta> a) else
+   if \<exists>a. u = \<eta>' a then \<rho>' (SOME a. u = \<eta>' a) else Ector (Gsub \<delta> id (Gmap (\<lambda>rec. rec (\<delta>, \<rho>, \<rho>')) (\<lambda>rec. rec (\<delta>, \<rho>, \<rho>')) u))"
+lemmas Esub_defs = Esub_Pperm_def Esub_PVrs_def Esub_Ector'_def
 
 sublocale Esub: Bimodel where
   Pperm = Esub_Pperm and
@@ -104,9 +95,7 @@ sublocale Esub: Bimodel where
   Eperm = Eperm and
   EVrs = EVrs and
   Ector = Ector and
-  is_base = Esub_is_base and
-  Ector_base = Esub_Ector_base and
-  Ector_step = Esub_Ector_step
+  Ector' = Esub_Ector'
   apply standard
   apply (unfold Esub_defs)
   subgoal for \<sigma> \<tau>
@@ -133,17 +122,22 @@ locale Birecursor_Sub = Birecursor where
   for Eperm EVrs Ector
 begin
 
-definition "Esub \<delta> \<rho> \<rho>' e = rec Esub_Pperm Esub_PVrs Esub_is_base Esub_Ector_base Esub_Ector_step e (\<delta>, \<rho>, \<rho>')"
+definition "Esub \<delta> \<rho> \<rho>' e = rec Esub_Pperm Esub_PVrs Esub_Ector' e (\<delta>, \<rho>, \<rho>')"
 
 sublocale Esub: Substitution Eperm EVrs Ector Esub
   apply standard
       apply (unfold Esub_def)
-      apply (subst rec_base[OF Esub.Bimodel_axioms]; auto simp add: eta_inject Esub_defs)
-      apply (subst rec_base[OF Esub.Bimodel_axioms]; auto simp add: eta_distinct' eta'_inject Esub_defs)
-    apply (subst rec_Ector[OF Esub.Bimodel_axioms]; auto simp add: G.Map_comp[THEN fun_cong, simplified] comp_def Esub_defs)
+      apply (subst rec_Ector[OF Esub.Bimodel_axioms]; auto simp add:
+         eta_natural[of id id, unfolded G.Sb_Inj, simplified]
+         eta_inject Esub_defs)
+      apply (subst rec_Ector[OF Esub.Bimodel_axioms]; auto simp add:
+         eta'_natural[of id id, unfolded G.Sb_Inj, simplified]
+         eta_distinct' eta'_inject Esub_defs)
+      apply (subst rec_Ector[OF Esub.Bimodel_axioms]; auto dest: eta_inversion[of id id, unfolded G.Sb_Inj, simplified] eta'_inversion[of id id, unfolded G.Sb_Inj, simplified] simp add:
+         eta_distinct eta_inject eta_distinct' eta'_inject G.Map_comp[THEN fun_cong, simplified] comp_def Esub_defs)
    apply (rule order_trans[OF rec_EVrs[OF Esub.Bimodel_axioms]]; simp add: Esub_defs)
   apply (subst rec_EPerm[OF Esub.Bimodel_axioms]; simp add: Esub_defs)
-  apply (rule arg_cong[where f = "\<lambda>p. Eperm _ (rec _ _ _ _ _ _ p)"])
+  apply (rule arg_cong[where f = "\<lambda>p. Eperm _ (rec _ _ _ _ p)"])
   apply (auto simp: fun_eq_iff)
   apply (metis Int_Un_empty Int_emptyD bij_inv_rev imsupp_idle2 not_in_imsupp_same)
   apply (metis (mono_tags, lifting) Int_Un_empty Int_Un_emptyI1 bij_betw_inv_into imsupp_inv inv_simp1
